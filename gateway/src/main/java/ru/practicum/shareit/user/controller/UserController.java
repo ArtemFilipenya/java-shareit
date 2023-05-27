71b609ec7b2abfc1af8350b30e43e8e03ad6aa96
Artem Filipenya <as.filipenya@gmail.com>
2023-05-27 21:23:32 +0300

    v5.2

--- gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -61,7 +61,16 @@ public class UserController {
     }
 
     public boolean isValidEmailAddress(String email) {
-        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-        return !email.matches(emailRegex);
+        String ePattern = "(?:[A-Za-z0-9!#$%&'*+/=?.^_`{|}~]" +
+                "+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~]+)*|" +
+                "\\\"(?:[x01-x08x0bx0cx0e-x1fx21x23-x5bx5d-x7f]|[x01-x09x0bx0cx0e-x7f])*\\\")" +
+                "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)" +
+                "+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|" +
+                "[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])).){3}(?:(2(5[0-5]|" +
+                "[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[x01-x08x0bx0cx0e-x1fx21-x5ax53-x7f]" +
+                "|[x01-x09x0bx0cx0e-x7f])+)])";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
     }
 }
